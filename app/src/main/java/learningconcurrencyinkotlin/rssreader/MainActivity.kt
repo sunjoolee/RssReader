6111ac4581b259e2add3882128dbe0dabfde5712
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2023-02-03 12:42:05 +0900

    CH2 스레드 생성
    디스패처에 코루틴 붙이기

--- app/src/main/java/learningconcurrencyinkotlin/rssreader/MainActivity.kt
+++ app/src/main/java/learningconcurrencyinkotlin/rssreader/MainActivity.kt
@@ -2,10 +2,19 @@ package learningconcurrencyinkotlin.rssreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
 
 class MainActivity : AppCompatActivity() {
+    val dispatcher = newSingleThreadContext(name = "ServiceCall")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch(dispatcher){
+            //TODO Call coroutine here
+        }
     }
 }
