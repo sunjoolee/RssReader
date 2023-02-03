bc3d4184bba4f8ddc4ab77e957d16ee06d9019c3
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2023-02-03 12:57:19 +0900

    CH2 서비스 호출을 위한 코루틴 생성

--- app/src/main/java/learningconcurrencyinkotlin/rssreader/MainActivity.kt
+++ app/src/main/java/learningconcurrencyinkotlin/rssreader/MainActivity.kt
@@ -5,16 +5,33 @@ import android.os.Bundle
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import java.nio.file.NotDirectoryException
+import javax.xml.parsers.DocumentBuilderFactory
 
 class MainActivity : AppCompatActivity() {
-    val dispatcher = newSingleThreadContext(name = "ServiceCall")
+    private val dispatcher = newSingleThreadContext(name = "ServiceCall")
+    private val factory = DocumentBuilderFactory.newInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         GlobalScope.launch(dispatcher){
-            //TODO Call coroutine here
+            val headlines = fetchRssHeadLines()
         }
+    }
+
+    private fun fetchRssHeadLines() : List<String>{
+        val builder = factory.newDocumentBuilder()
+        val xml = builder.parse("https://www.npr.org/rss/rss.php?id=1001")
+        val news = xml.getElementsByTagName("channel").item(0)
+        return (0 until news.childNodes.length)
+            .map { news.childNodes.item(it) }
+            .filter { Node.ELEMENT_NODE == it.nodeType }
+            .map{it as Element}
+            .filter { "item" == it.tagName }
+            .map { it.getElementsByTagName("title").item(0).textContent }
     }
 }
