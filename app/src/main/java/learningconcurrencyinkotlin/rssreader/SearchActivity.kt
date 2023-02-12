86f25d836726879974912ef8113c994024eedf21
sunjoolee <spottypoo124@gmail.com>
2023-02-12 15:27:21 +0900

    CH6 채널 인 액션
    검색 액티비티 추가

---
+++ app/src/main/java/learningconcurrencyinkotlin/rssreader/SearchActivity.kt
@@ -0,0 +1,12 @@
+package learningconcurrencyinkotlin.rssreader
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SearchActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
