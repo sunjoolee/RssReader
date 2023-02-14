208159fe83fa183ab1b5b8519ed20775358187e9
sunjoolee <spottypoo124@gmail.com>
2023-02-14 21:13:26 +0900

    CH7 액터 인 액션
    UI가 업데이트에 반응하도록 채널 추가

--- app/src/main/java/learningconcurrencyinkotlin/rssreader/search/ResultsCounter.kt
+++ app/src/main/java/learningconcurrencyinkotlin/rssreader/search/ResultsCounter.kt
@@ -1,12 +1,19 @@
 package learningconcurrencyinkotlin.rssreader.search
 
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.channels.actor
 
 object ResultsCounter {
     private val counterContext = newSingleThreadContext("counter")
     private var counter = 0
+
+    private val notifications = Channel<Int>(Channel.CONFLATED)
+
+    //UI에 채널을 노출하는 함수
+    fun getNotificationChannel() : ReceiveChannel<Int> = notifications
 
     private val counterActor = GlobalScope.actor<Void?>(counterContext){
         for (msg in channel){
