4fc00a26f51a48b1c6e0fbbc3a14ec137e65ca50
sunjoolee <spottypoo124@gmail.com>
2023-02-14 21:31:27 +0900

    CH7 액터 인 액션
    카운터 재설정을 위한 액터 확장

--- app/src/main/java/learningconcurrencyinkotlin/rssreader/search/ResultsCounter.kt
+++ app/src/main/java/learningconcurrencyinkotlin/rssreader/search/ResultsCounter.kt
@@ -6,6 +6,9 @@ import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.channels.actor
 
+enum class Action{
+    INCREASE, RESET
+}
 object ResultsCounter {
     private val counterContext = newSingleThreadContext("counter")
     private var counter = 0
@@ -15,12 +18,16 @@ object ResultsCounter {
     //UI에 채널을 노출하는 함수
     fun getNotificationChannel() : ReceiveChannel<Int> = notificationChannel
 
-    private val counterActor = GlobalScope.actor<Void?>(counterContext){
+    private val counterActor = GlobalScope.actor<Action>(counterContext){
         for (msg in channel){
-            counter++
+            when(msg){
+                Action.INCREASE -> counter++
+                Action.RESET -> counter = 0
+            }
             notificationChannel.send(counter)
         }
     }
 
-    suspend fun increment() = counterActor.send(null)
+    suspend fun increment() = counterActor.send(Action.INCREASE)
+    suspend fun reset() = counterActor.send(Action.RESET)
 }
