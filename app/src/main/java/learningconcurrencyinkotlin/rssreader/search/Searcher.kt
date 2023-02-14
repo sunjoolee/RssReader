bb2f9aca0305dfc28caa9a8982623e4387bcdcb4
sunjoolee <spottypoo124@gmail.com>
2023-02-14 19:04:54 +0900

    CH6 채널 인 액션
    협업 검색 구현하기

--- app/src/main/java/learningconcurrencyinkotlin/rssreader/search/Searcher.kt
+++ app/src/main/java/learningconcurrencyinkotlin/rssreader/search/Searcher.kt
@@ -2,10 +2,60 @@ package learningconcurrencyinkotlin.rssreader.search
 
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.newFixedThreadPoolContext
 import learningconcurrencyinkotlin.rssreader.model.Article
+import learningconcurrencyinkotlin.rssreader.model.Feed
+import learningconcurrencyinkotlin.rssreader.producer.ArticleProducer
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import javax.xml.parsers.DocumentBuilderFactory
 
 class Searcher {
+    private val dispatcher = newFixedThreadPoolContext(3, "IO")
+    private val factory = DocumentBuilderFactory.newInstance()
+
+    private val feeds = listOf(
+        Feed("npr", "https://www.npr.org/rss/rss.php?id=1001"),
+        Feed("cnn", "http://rss.cnn.com/rss/cnn_topstories.rss")
+        // Feed("fox","http://feeds.foxnews.com/foxnews/politics?format=xml")
+    )
+
+    //쿼리를 받아서 ReceiveChannel<Article>을 반환하는 공개 함수
     fun search(query: String): ReceiveChannel<Article> {
         return Channel(150)
+    }
+
+    //피드, 쿼리, 채널을 갖고 실제 검색을 하는 비공개 함수
+    private suspend fun search(
+        feed: Feed,
+        channel: SendChannel<Article>,
+        query: String){
+
+            val builder = factory.newDocumentBuilder()
+            val xml = builder.parse(feed.url)
+            val news = xml.getElementsByTagName("channel").item(0)
+
+            return (0 until news.childNodes.length)
+                .map { news.childNodes.item(it) }
+                .filter { Node.ELEMENT_NODE == it.nodeType }
+                .map { it as Element }
+                .filter { "item" == it.tagName }
+                .forEach {
+                    val title = it.getElementsByTagName("title").item(0).textContent
+
+                    var summary = it.getElementsByTagName("description").item(0).textContent
+
+                    //모든 컨텐츠를 매핑하는 대신 필터링 한 기사를 채널을 통해 전송
+                    if(title.contains(query) || summary.contains(query)) {
+
+                        if (!summary.startsWith("<div") && summary.contains("<div")) {
+                            summary = summary.substring(0, summary.indexOf("<div"))
+                        }
+
+                        val article = Article(feed.name, title, summary)
+                        channel.send(article)
+                    }
+                }
     }
 }
